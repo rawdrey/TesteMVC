8923707e6236284d9c3c7ee0328a98af2ba87c74
Rawdrey <rawdrey@gmail.com>
2024-06-28 17:47:14 -0300

    niciando projeto mvc

---
+++ Models/modelo.cs
@@ -0,0 +1,8 @@
+﻿
+public int ID_Cliente { get; set; }
+public string Nome { get; set; }
+public string Endereco { get; set; }
+public string Telefone { get; set; }
+public string RG { get; set; }
+public string CPF { get; set; }
+public string Email { get; set; }
